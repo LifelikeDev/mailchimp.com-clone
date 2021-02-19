7707ad65229164b3a09c367070d5342467e0d2ff
Navian <primeguyy@gmail.com>
2021-02-19 23:30:50 +0000

    Footer and additional changes to other sections added

--- js/script.js
+++ js/script.js
@@ -3,17 +3,20 @@ const topmostForm = document.getElementById('topmost-form');
 const closeSearch = document.querySelector('.search-delete');
 const headerBarWrapper = document.querySelector('.header-bar__wrapper');
 const headerBarLayout = document.querySelector('.header-bar__layout');
-
+const year = document.getElementById('year');
 
 // adding event listener to search icon to display topmost search section 
 navSearch.addEventListener('click', () => {
 
     topmostForm.style.display = 'block';
+    headerBarLayout.style.backgroundColor = 'var(--link-text-color)'
 });
 
 closeSearch.addEventListener('click', () => {
 
     topmostForm.style.display = 'none';
+    headerBarLayout.style.backgroundColor = 'var(--header-primary-color)';
+
 });
 
 // change header bar color when page is scrolled downwards
@@ -31,3 +34,9 @@ headerBarWrapper.addEventListener('mouseenter', () => {
 headerBarWrapper.addEventListener('mouseleave', () => {
     headerBarWrapper.classList.remove('hovered');
 });
+
+// adding dynamic date to developer's copyright info
+const date = new Date();
+const currentYear = date.getFullYear();
+
+year.innerHTML = currentYear;
